77270e89759a453038bce8cb7f1abd9dbae99535
Hospilink-AI <hospilinkai@gmail.com>
2026-05-22 16:48:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 const http = require('http');
 const app = require('./src/app');
 const connectDB = require('./src/config/database');
